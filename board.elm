d96003a03343a8274006cefad16a3dcf4d04d7f5
Karthi <karthikan293@gmail.com>
2017-02-08 11:16:20 +0100

    affichage map

--- board.elm
+++ board.elm
@@ -22,23 +22,23 @@ type Color = Black | White
 update : Msg -> Model -> Model
 update msg model = model
 
-f : Int -> Int -> Svg Msg
-f a b =  line [ x1 (toString <| (a+1)*40 ), y1 (toString <| (b+1)*40  ), x2 (toString <| (a+19)*40 ), y2 (toString <| (b+ 1)*40 ), stroke "black" , strokeWidth "2" ] []
+ligneHorizontal : Int -> Int -> Svg Msg
+ligneHorizontal a b =  line [ x1 (toString <| (a+1)*40 ), y1 (toString <| (b+1)*40  ), x2 (toString <| (a+19)*40 ), y2 (toString <| (b+ 1)*40 ), stroke "black" , strokeWidth "2" ] []
 
-g : Int -> Int -> Svg Msg
-g a b =  line [ x1 (toString <| (a+1)*40 ), y1 (toString <| (b+1)*40  ), x2 (toString <| (a+ 1)*40 ), y2 (toString <| (b+19)*40 ), stroke "black" , strokeWidth "2" ] []
+ligneVertical : Int -> Int -> Svg Msg
+ligneVertical a b =  line [ x1 (toString <| (a+1)*40 ), y1 (toString <| (b+1)*40  ), x2 (toString <| (a+ 1)*40 ), y2 (toString <| (b+19)*40 ), stroke "black" , strokeWidth "2" ] []
 
-h: (Int, Int, String) -> Svg Msg
-h (a, b, color) =  circle [ cx ( toString <| (a)*45 ) , cy (toString <| (b)*45) , r (toString 15),fill color ] []
+tracerCercle: (Int, Int, String) -> Svg Msg
+tracerCercle (a, b, color) =  circle [ cx ( toString <| (a)*45 ) , cy (toString <| (b)*45) , r (toString 15),fill color ] []
 
-m: Int -> Int -> Svg Msg
-m a b = rect [fill "#77a777", x (toString <| (a+1)*40 ),y (toString <| (b+1)*40 ), width "40px", height "40px" ] []
+tracerCarre: Int -> Int -> Svg Msg
+tracerCarre a b = rect [fill "#77a777", x (toString <| (a+1)*40 ),y (toString <| (b+1)*40 ), width "40px", height "40px" ] []
 
 --m: (Int, Int, String) -> Svg Msg
 --model.board =List.map h [ (4,4,"#efefef"),(4,5,"#020202"),(5,4,"#efefef"),(5,5,"#020202") ]
 
 recursion: Int -> Int -> List(Svg Msg)
-recursion a b = if a==0 && b==0 then [(m a b)] else if a == 0 then [(m a b)] ++ (recursion 7 (b-1)) else [(m a b)] ++ (recursion (a-1) b)
+recursion a b = if a==0 && b==0 then [(tracerCarre a b)] else if a == 0 then [(tracerCarre a b)] ++ (recursion 7 (b-1)) else [(tracerCarre a b)] ++ (recursion (a-1) b)
 
 view: Model -> Html Msg
 view model =
@@ -50,13 +50,13 @@ view model =
        -- [ rect [ width "500", height "500", fill "#77a777"] []
       --  ] ++
        (recursion 7 7) ++
-        ( List.map (\c -> f 0 c) <| List.range 0 8 ) ++
-        ( List.map (\c -> g c 0) <| List.range 0 8 ) ++
+        ( List.map (\c -> ligneHorizontal 0 c) <| List.range 0 8 ) ++
+        ( List.map (\c -> ligneVertical c 0) <| List.range 0 8 ) ++
         --( List.map (\c -> m 0 c) <| List.range 0 8 ) ++
 
     --    (List.map (\d -> List.map (\c -> m d c) <| List.range 0 8 ) <| List.range 0 8) ++
      --   ( List.map (\c -> g c 0) <| List.range 0 8 )++
-       ( List.map h [ (4,4,"#efefef"),(4,5,"#020202"),(5,4,"#efefef"),(5,5,"#020202") ] )
+       ( List.map tracerCercle [ (4,4,"#efefef"),(4,5,"#020202"),(5,4,"#efefef"),(5,5,"#020202") ] )
       ]
    --, button [ onClick ( ) ] [ Html.text ("toto") ]
     ]
